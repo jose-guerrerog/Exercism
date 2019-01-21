af11fa4bca5497915abbbe821fe30958ea4bccdd
jose-guerrero <jose-guerrero@outlook.com>
2019-01-21 12:26:40 +1100

    Removing unnecessary comments

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,20 +1,11 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package twofer
 
-import "fmt"
+import "strings"
 
-// ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
+	defaultText := "One for you, one for me."
 	if name == "" {
-		return "One for you, one for me."
+		return defaultText
 	}
-	return fmt.Sprintf("One for %s, one for me.", name)
+	return strings.Replace(defaultText, "you", name, -1)
 }
